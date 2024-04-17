6f532310005cb567e67a6d2fffcc1f9a615350fe
mkasiulis <m.kasiulis@gmail.com>
2024-04-17 17:17:29 -0400

    Slight changes to script

--- compute_radial_correlations.py
+++ compute_radial_correlations.py
@@ -5,16 +5,13 @@ import matplotlib.colors as clr
 import cmasher as cmr
 import hickle as hkl
 import sys
+import os
 
-# points = np.random.rand(10000,2)
-# points = hkl.load("/home/mathias/quasiquasi7crystal2d_K100 (2).hkl")[:,:-1]
-# points = np.loadtxt("/home/mathias/quasicrystal7deBruijndual.csv")
-points = np.loadtxt("/home/mathias/Documents/remote/hard_disk_tests_Shen/Phi0.7200/001/dump.txt")[:,0:2]
-# points = hkl.load("/home/mathias/Documents/snek/HPY2D_phi0.6_a0.0_N262144_K256_points_0.hkl")[:,:-1]
-# points = hkl.load("/home/mathias/Documents/snek/HPY2D_phi0.6_a0.0_N50000000_K5050.0_points_0.hkl")[:,:-1]
-# points = hkl.load("/home/mathias/Documents/snek/HPY3D_phi0.25_a0.0_N1000000_K64_points_0.hkl")[:,:-1]
-# plt.scatter(points[:,0], points[:,1])
-# plt.show()
+file_path = sys.argv[1]
+[dname,file_name] = os.path.split(file_path)
+dname = os.path.abspath(dname)
+
+points = hkl.load(dname+file_name)[:,:-1]
 
 ndim = points.shape[1]
 npoints = points.shape[0]
@@ -33,9 +30,22 @@ if ndim == 2:
     
     boops = rust.compute_2d_boops(points, boop_orders, boxsize, periodic)
     print(boops.shape)
-    radial_rdf, g7 = rust.compute_radial_correlations_2d(points, boops[:,-1,:], boxsize, binsize, periodic)
+    
+    _, gboop = rust.compute_radial_correlations_2d(points, boops[:,-1,:], boxsize, binsize, periodic)
+    
+    nK =100
+    K = 2.0 * np.pi * nK / boxsize
+    
+    translational_x = np.cos(K*points[:,0])
+    translational_y = np.sin(K*points[:,0])
+    translational = np.vstack([translational_x,translational_y]).T
+    
+    np.savetxt(file_name+"_translational_"+str(nK)+".csv", translational)
+    
+    radial_rdf, corr = rust.compute_radial_correlations_2d(points, translational, boxsize, binsize, periodic)
+
     print(radial_rdf.shape)
-    print(g7.shape)
+    # print(g7.shape)
 elif ndim == 3:
     print("Not implemented in 3d for now")
 else: 
@@ -45,22 +55,44 @@ nbins = radial_rdf.shape[0]
 bins = (np.arange(0, nbins) + 0.5)*binsize
 print(bins.shape)
 
-np.savetxt("radial_rdf_test.csv", np.vstack([bins,radial_rdf]))
+#np.savetxt("points.csv", points)
+np.savetxt(file_name+"_radial_rdf_test.csv", np.vstack([bins,radial_rdf]).T)
+np.savetxt(file_name+"_radial_corr_test.csv", np.vstack([bins,corr[:,0]+corr[:,1]]).T)
+data = np.loadtxt(file_name+"_radial_rdf_test.csv")
+bins = data[:,0]
+radial_rdf = data[:,1]
 
-fig = plt.figure(figsize=(10,10))
+fig = plt.figure()#figsize=(10,10))
 ax = fig.gca()
-pc = ax.plot(bins, radial_rdf)
-plt.savefig("radial_rdf_test.png", dpi = 300)
+pc = ax.plot(bins, radial_rdf,c=cmr.ember(0.5), label = 'Minimized System')
+#pc = ax.plot(r/Nk, g*(r/Nk>=2*rad),c='grey',linestyle='dashed', label='Percus-Yevick')
+ax.set_xlim(0,0.5)
+ax.legend(loc='upper right',fontsize=24)
+pc = ax.plot(bins, radial_rdf,c=cmr.ember(0.5))
+ax.tick_params(labelsize=18)
+ax.set_xlabel(r"$r$",fontsize=18)
+ax.set_ylabel(r"$g(r)$",fontsize=18)
+plt.savefig(file_name+"_radial_rdf_test.png", bbox_inches = 'tight',pad_inches = 0, dpi = 300)
 plt.close()
 
 fig = plt.figure(figsize=(10,10))
 ax = fig.gca()
-pc = ax.plot(bins, g7[:,0]+g7[:,1])
-plt.savefig("radial_g7_test.png", dpi = 300)
+pc = ax.plot(bins, corr[:,0]+corr[:,1])
+ax.set_xlim(0,0.5)
+plt.savefig(file_name+"_radial_corr_"+str(nK)+"_test.png", dpi = 300)
 plt.close()
+
+np.savetxt(file_name+"_radial_gboop_test.csv", np.vstack([bins,gboop[:,0]+gboop[:,1]]).T)
 
 fig = plt.figure(figsize=(10,10))
 ax = fig.gca()
-pc = ax.scatter(boops[:,-1,0], boops[:,-1,1])
-plt.savefig("psi7_test.png", dpi = 300)
+pc = ax.plot(bins, gboop[:,0]+gboop[:,1])
+ax.set_xlim(0,0.1)
+plt.savefig(file_name+"_radial_gboop_test.png", dpi = 300)
 plt.close()
+
+# fig = plt.figure(figsize=(10,10))
+# ax = fig.gca()
+# pc = ax.scatter(boops[:,-1,0], boops[:,-1,1])
+# plt.savefig("psi6_"+str(phi)+"_test.png", dpi = 300)
+# plt.close()
--- compute_vector_rdf.py
+++ compute_vector_rdf.py
@@ -5,14 +5,26 @@ import matplotlib.colors as clr
 import cmasher as cmr
 import hickle as hkl
 import sys
+import os
 
-points = np.random.rand(100000,2)
-points = hkl.load("/home/mathias/quasiquasi7crystal2d_K100 (2).hkl")[:,:-1]
-# points = hkl.load("/home/mathias/Documents/snek/HPY2D_phi0.6_a0.0_N262144_K256_points_0.hkl")[:,:-1]
-# points = hkl.load("/home/mathias/Documents/snek/HPY2D_phi0.6_a0.0_N50000000_K5050.0_points_0.hkl")[:,:-1]
-# points = hkl.load("/home/mathias/Documents/snek/HPY3D_phi0.25_a0.0_N1000000_K64_points_0.hkl")[:,:-1]
-# plt.scatter(points[:,0], points[:,1])
-# plt.show()
+def cut_circle(r, rad=0.5):
+    idx = np.nonzero(np.linalg.norm(r,axis=-1)<=rad)
+    r = np.squeeze(r[idx])
+    return r
+
+file_path = sys.argv[1]
+[dname,file_name] = os.path.split(file_path)
+dname = os.path.abspath(dname)
+
+points = hkl.load(dname+file_name)[:,:-1]
+
+# points = hkl.load("/home/mathias/Documents/snek/hyperalg-master/HPY2D/phi0.6/a-3.0/HPY2D_phi0.6_a-3.0_N50000000_K5050.0_points_0.hkl")[:,:-1]
+points -= np.mean(points)
+# points = cut_circle(points, rad=0.01)
+points /= np.amax(points)
+points *= 0.5
+# print(np.amax(points))
+# np.savetxt("excerpt_rose.csv", points)
 
 ndim = points.shape[1]
 npoints = points.shape[0]
@@ -22,6 +34,7 @@ radius = boxsize / (npoints)**(1.0/ndim)
 binsize = radius / 20.0
 periodic = False
 logscaleplot = False
+vmaxmax = 10
 
 if ndim == 2:
     vector_rdf = rust.compute_vector_rdf2d(points, boxsize, binsize, periodic)
@@ -34,7 +47,7 @@ nbins = np.ceil(boxsize/binsize)
 rho_n = npoints * npoints / ( boxsize * boxsize)
 vector_rdf /= rho_n * binsize * binsize
     
-np.savetxt("vector_rdf_test.csv", vector_rdf)
+np.savetxt(file_name+"vector_rdf_test.csv", vector_rdf)
 
 if periodic:
     center = int(vector_rdf.shape[0]/2)
@@ -48,15 +61,15 @@ ax = fig.gca()
 if logscaleplot:
     pc = ax.imshow(vector_rdf[center-width:center+width+1, center-width:center+width+1],norm=clr.LogNorm(vmin=1e-3,vmax=1e1), cmap=cmr.ember)
 else:
-    vmax = np.min([vector_rdf.max(), 10])
+    vmax = np.min([vector_rdf.max(), 2])
     pc = ax.imshow(vector_rdf[center-width:center+width+1, center-width:center+width+1], vmin = 0, vmax = vmax, cmap=cmr.ember)
 fig.colorbar(pc)
-plt.savefig("vector_rdf_test.png", dpi = 300)
+plt.savefig(file_name+"_vector_rdf_test.png", dpi = 300)
 plt.close()
 
 width = int(vector_rdf.shape[1]/10)
 fig = plt.figure(figsize=(10,10))
 ax = fig.gca()
 pc = ax.plot(vector_rdf[center, center-width:center+width+1])
-plt.savefig("vector_rdf_test_centerline.png", dpi = 300)
+plt.savefig(file_name +"_vector_rdf_test_centerline.png", dpi = 300)
 plt.close()
